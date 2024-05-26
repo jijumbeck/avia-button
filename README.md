21d878f95d17b70397a63bb4b365c263defc5ead
sonya <jijumbeck@gmail.com>
2024-05-26 16:22:21 +0300

    set up project

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Проект-шаблон сборки для React проекта на Typescript
+
+## Описание
+
+Сборка включает в себя:
+
+* Запуск проекта в браузере;
+* Резолв CSS файлов;
+* Резолв Typescript;
+
+## Запуск
+
+```bash
+
+# Запуск приложения в браузере.
+
+$  npm  run  start
+
+# Build в development mode.
+
+$  npm  run  build:dev
+
+# Build в production mode.
+
+$  npm  run  start:prod
+
+```
---
+++ webpack.config.ts
@@ -0,0 +1,67 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import path from 'path';
+import webpack from 'webpack';
+
+type Mode = 'production' | 'development';
+
+type EnvVariabales = {
+  mode: Mode,
+  port: number
+}
+
+export default (env: EnvVariabales) => {
+  const isDev = env.mode === 'development';
+
+  return {
+    mode: env.mode || 'development',
+
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true
+    },
+
+    devtool: isDev ? 'inline-source-map' : false,
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      !isDev && new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css'
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader"
+          ],
+        },
+        {
+          test: /\.tsx?.$/,
+          use: 'ts-loader',
+          exclude: /node-modules/
+        }
+      ]
+    },
+
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+
+    devServer: {
+      port: env.port || 5000,
+      open: true
+    }
+
+  } as webpack.Configuration;
+}
+
+
