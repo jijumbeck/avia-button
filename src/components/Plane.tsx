9aa1c9baf8ba3f4be5806450a23d5fbba016edb5
sonya <jijumbeck@gmail.com>
2024-05-30 23:01:37 +0300

    feat Plane component

--- src/components/Plane.tsx
+++ src/components/Plane.tsx
@@ -1,32 +1,39 @@
 import React from "react";
 
-export function Plane() {
+export function Plane({ color }: { color?: string }) {
+    const planeColor = color && color.match(/#[0-9a-f]{3,8}/) ? color : '#000';
+
     return (
         <svg
             width='100px' height='100px'
         >
             <line
                 x1='80' y1='20' x2='20' y2='80'
-                strokeWidth='15' stroke="#000"
+                strokeWidth='15' stroke={planeColor}
                 strokeLinecap="round"
             />
-            {/* <polygon 
-                points='0 10, 0 20, 50 20, 50 0'
-            /> */}
             <g>
                 <polygon
                     points='0 30, 50 60, 70 40, 10 20'
+                    stroke={planeColor}
+                    fill={planeColor}
                 />
                 <polygon
                     points='70 100, 40 50, 60 30, 80 90'
+                    stroke={planeColor}
+                    fill={planeColor}
                 />
             </g>
             <g>
-                <polygon 
+                <polygon
                     points="0 80, 5 70, 30 70, 20 87"
+                    stroke={planeColor}
+                    fill={planeColor}
                 />
-                <polygon 
+                <polygon
                     points="20 100, 30 95, 30 70, 13 80"
+                    stroke={planeColor}
+                    fill={planeColor}
                 />
             </g>
         </svg>
